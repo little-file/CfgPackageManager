005e133d15b7825eec343073ada09516849a9c76
little-file <fakeaiqqff@gmail.com>
2025-03-29 12:58:07 +0000

    added dnf and flatpak yaml not ready

--- src/main.py
+++ src/main.py
@@ -60,6 +60,153 @@ def yaml_read():
     except yaml.YAMLError as e:
         print(f"YAML error read: {e}")
 
+# DNF
+def dnf_update():
+    import subprocess
+    makecache = "sudo dnf makecache"
+    command = "sudo dnf update -y"
+    try:
+        print("DNF cache starting...")
+        result = subprocess.run(makecache, shell=True, check=True, capture_output=True, text=True)
+        print("DNF cache finished.")
+        if result.stdout:
+            print("DNF cache OUT:")
+            print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: DNF cache during a problem.")
+        print(f"Rrror code: {e.returncode}")
+        if e.stderr:
+            print(f"Error OUT:\n{e.stderr}")
+    except FileNotFoundError:
+        print("Error: 'dnf' not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+    try:
+        print("system updates starting...")
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        print("system updates finished.")
+        if result.stdout:
+            print("Updates OUT:")
+            print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: System updates during a problem.")
+        print(f"Rrror code: {e.returncode}")
+        if e.stderr:
+            print(f"Error OUT:\n{e.stderr}")
+    except FileNotFoundError:
+        print("Error: 'dnf' not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+def dnf_mirror():
+    import subprocess
+    command = "sudo dnf config-manager --add-repo"
+    try:
+        print("system mirror starting...")
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        print("system mirror finished.")
+        if result.stdout:
+            print("mirror OUT:")
+            print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: System mirror during a problem.")
+        print(f"Rrror code: {e.returncode}")
+        if e.stderr:
+            print(f"Error OUT:\n{e.stderr}")
+    except FileNotFoundError:
+        print("Error: 'dnf' not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+def dnf_install():
+    import subprocess
+    command = "sudo dnf config-manager --add-repo"
+    try:
+        print("system installs starting...")
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        print("system installs finished.")
+        if result.stdout:
+            print("installs OUT:")
+            print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: System installs during a problem.")
+        print(f"Rrror code: {e.returncode}")
+        if e.stderr:
+            print(f"Error OUT:\n{e.stderr}")
+    except FileNotFoundError:
+        print("Error: 'dnf' not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def dnf_system():
+    dnf_mirror()
+    dnf_update()
+    dnf_install()
+
+# FLATPAK
+def flatpak_update():
+    import subprocess
+    command = "flatpak update -y"
+    try:
+        print("flatpak updates starting...")
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        print("flatpak updates finished.")
+        if result.stdout:
+            print("Updates OUT:")
+            print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: flatpak updates during a problem.")
+        print(f"Rrror code: {e.returncode}")
+        if e.stderr:
+            print(f"Error OUT:\n{e.stderr}")
+    except FileNotFoundError:
+        print("Error: 'flatpak' not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+def flatpak_mirror():
+    import subprocess
+    command = "flatpak remote-add --if-not-exists --user"
+    try:
+        print("flatpak mirror starting...")
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        print("flatpak mirror finished.")
+        if result.stdout:
+            print("Mirror OUT:")
+            print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: flatpak Mirror during a problem.")
+        print(f"Rrror code: {e.returncode}")
+        if e.stderr:
+            print(f"Error OUT:\n{e.stderr}")
+    except FileNotFoundError:
+        print("Error: 'flatpak' not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+def flatpak_install():
+    import subprocess
+    command = "flatpak install -y --user"
+    try:
+        print("flatpak installing starting...")
+        result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+        print("flatpak installing finished.")
+        if result.stdout:
+            print("installing OUT:")
+            print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: flatpak installing during a problem.")
+        print(f"Rrror code: {e.returncode}")
+        if e.stderr:
+            print(f"Error OUT:\n{e.stderr}")
+    except FileNotFoundError:
+        print("Error: 'flatpak' not found.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
 def frist():
     os_reaction()
     if os_pending == False:
